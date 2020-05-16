ec3947efba0b61787a3d6a6703bbd88f7133c9ed
Z芊芊 <you@example.com>
2020-05-16 20:37:31 +0800

    first commit

---
+++ codes running on dataset2/TRIGRS-NP code/11.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module unload MPI
+module load MPI/mpich/3.2.1-gcc-4.8.5-dynamic
+module load gsl/2.4-gcc-4.8.5
---
+++ codes running on dataset2/TRIGRS-NP code/run1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#export OMP_NUM_THREADS=1
+yhrun -N 32 -n 32 -c 8 ./prg
